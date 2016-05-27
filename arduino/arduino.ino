013aba92dd40ff369b2a591e1322b291a8902e17
Pascal Gremaud <polchky-unifr>
2016-05-27 10:54:55 +0200

    add ProjectModule class

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,13 +1,17 @@
 #include <Adafruit_NeoPixel.h>
+#include <ProjectModule.h>
 
-#define NSTRIPS 10
-#define NPIXELS 16
+
+const int N_MODULES = 4;
+const int N_STRIP_LEDS = 11;
+const int N_RING_LEDS = 24;
 
 const char ORDER_DELIMITER = '/';
 const String ORDER_SETUP = "s";
 const String ORDER_DEV = "d";
 const String ORDER_RESET = "r";
 const String ORDER_ADD = "a";
+const String ORDER_CHECK = "c";
 
 const String STATE_IDLE = "IDLE";
 const String STATE_COMMIT = "COMMIT";
@@ -15,26 +19,13 @@ const String STATE_SELECTING = "SELECTING";
 const String STATE_REPLAYING = "REPLAYING";
 const String STATE_RECEIVING = "RECEIVING";
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strips[NSTRIPS] = {
-  Adafruit_NeoPixel(NPIXELS, 4, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(NPIXELS, 5, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(NPIXELS, 6, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(NPIXELS, 7, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(NPIXELS, 8, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(NPIXELS, 9, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(NPIXELS, 10, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(NPIXELS, 11, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(NPIXELS, 12, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(NPIXELS, 13, NEO_GRB + NEO_KHZ800)
+ProjectModule modules[N_MODULES] = {
+  ProjectModule(4, N_STRIP_LEDS, 5, N_RING_LEDS),
+  ProjectModule(7, N_STRIP_LEDS, 6, N_RING_LEDS),
+  ProjectModule(8, N_STRIP_LEDS, 9, N_RING_LEDS),
+  ProjectModule(11, N_STRIP_LEDS, 10, N_RING_LEDS)
 };
+uint32_t myIndex = 0;
 
 String inputString = "";
 String currentState = STATE_IDLE;
@@ -58,28 +49,14 @@ String getInputStringNextPart(){
 
 void setup() {
   Serial.begin(9600);
-  for(uint16_t p=0; p<NSTRIPS; p++){
-    strips[p].begin();
-    strips[p].setBrightness(10);
-    strips[p].show();
-  }
 }
 
 void loop() {
-  uint32_t green = strips[0].Color(10,150,0);
-  uint32_t black = strips[0].Color(0,0,0);
-  uint32_t color;
-  for(uint16_t p=0; p<NSTRIPS; p++) {
-    for(uint16_t q=0; q<NPIXELS; q++){
-      if(q<=p){
-        color = green;
-      } else{
-        color = black;
-      }
-      strips[p].setPixelColor(q, color);
-    }
-    strips[p].show();
+  // Update each module
+  for (int i=0; i<N_MODULES; i++){
+    modules[i].update();
   }
+
 }
 
 /**
@@ -95,10 +72,7 @@ boolean checkInputString(){
 
 void executeOrder(){
   String orderType = getInputStringNextPart();
-  switch(orderType){
-    case ORDER_SETUP:
-      
-      break;
+  switch(1){
     default: 
       announceFaultyOrder();
       break;
@@ -111,12 +85,11 @@ void executeOrder(){
 void serialEvent() {
   if (Serial.available()) {
     inputString = Serial.readStringUntil('\n');
+    inputString.remove(inputString.length() - 1, 1);
   }
   if(checkInputString() == true){
     executeOrder();
     Serial.println("ok!");
-    //Serial.println(getInputStringNextPart());
-    //Serial.println(inputString);
   } else {
     announceFaultyOrder();
   }
