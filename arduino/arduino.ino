5a5d3c6b30d7b67a5d38dd5b9aafee7b1923c444
Pascal Gremaud <polchky-unifr>
2016-05-31 10:44:29 +0200

    add months blinking

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,53 +3,21 @@
 const int N_STRIP_LEDS = 11;
 
 const char ORDER_DELIMITER = '/';
-const char ORDER_RING = 'r'; // Used
+const char ORDER_RING = 'r'; // used
 const char ORDER_SETUP = 's';
 const char ORDER_DEV = 'd'; // used
 const char ORDER_RESET = 'x';
 const char ORDER_ADD = 'a';
 const char ORDER_COMMIT = 'c'; // used
 
-const int STATE_IDLE = 0; // used
-const int STATE_COMMIT = 1; // used
-const int STATE_SELECTING = 2; // used
-const int STATE_REPLAYING = 3; // used
-const int STATE_RECEIVING = 4;
+const uint8_t STATE_IDLE = 0; // used
+const uint8_t STATE_COMMIT = 1; // used
+const uint8_t STATE_SELECTING = 2; // used
+const uint8_t STATE_REPLAYING = 3; // used
+const uint8_t STATE_RECEIVING = 4; // used
 
 int BRIGHTNESS = 100;
 int DEFAULT_BRIGHTNESS = 100;
-
-int nDevs = 7;
-int currentState = STATE_IDLE;
-String inputString = "";
-unsigned long orderStart;
-int deltaT = 50;
-
-// Commit variables
-uint32_t commitColor;
-int commitModule;
-int commitRingIndex;
-int commitRingLength;
-int nCommits;
-unsigned long commitEndTime;
-
-uint32_t colors[7] = {
-  100,
-  25600,
-  6553600,
-  25700,
-  6553700,
-  6579200,
-  6579300
-};
-uint32_t black = 0;
-
-// Dev selection
-uint16_t analogLastValue;
-unsigned long analogLastMoved;
-uint8_t analogThreshold = 10;
-unsigned long analogTimeThreshold = 1000;
-uint8_t selectedDev;
 
 Adafruit_NeoPixel rings[4] = {
   Adafruit_NeoPixel(24, 6, false, 5, NEO_GRB + NEO_KHZ800),
@@ -67,6 +35,46 @@ Adafruit_NeoPixel strips[4] = {
 
 Adafruit_NeoPixel weeks = Adafruit_NeoPixel(16, 0, false, 12, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel months = Adafruit_NeoPixel(24, 0, false, 13, NEO_GRB + NEO_KHZ800);
+
+uint8_t nDevs = 7;
+uint8_t ledsPerDev = months.numPixels() / nDevs;
+uint8_t remaining = months.numPixels() - (ledsPerDev * nDevs);
+uint8_t selectedDev;
+  
+uint8_t currentState = STATE_IDLE;
+String inputString = "";
+unsigned long stateStart;
+uint8_t deltaT = 50;
+
+// Commit variables
+uint32_t commitColor;
+int commitModule;
+int commitRingIndex;
+int commitRingLength;
+int nCommits;
+unsigned long commitEndTime;
+
+// Commits replay variables
+uint8_t timeIndex;
+uint16_t commits[48][4];
+
+
+
+uint32_t colors[7] = {
+  100,
+  25600,
+  6553600,
+  25700,
+  6553700,
+  6579200,
+  6579300
+};
+
+// Dev selection
+uint16_t analogLastValue;
+unsigned long analogLastMoved;
+uint8_t ANALOG_THRESHOLD = 10;
+unsigned long ANALOG_TIME_THRESHOLD = 1000;
 
 
 
@@ -104,6 +112,8 @@ void switchState(int newState){
       clearRings();
       analogLastMoved = millis();
       break;
+    case STATE_RECEIVING:
+      break;
     case STATE_REPLAYING:
       clearRing("months");
       break;
@@ -114,7 +124,7 @@ void switchState(int newState){
       break;
     default: break;
   }
-  
+  stateStart = millis();
   currentState = newState;
 }
 
@@ -132,7 +142,6 @@ boolean checkInputString(){
 }
 
 void executeOrder(){
-  orderStart = millis();
   char order = getInputStringNextPart().charAt(0);
   switch(order){
     case ORDER_RING:
@@ -206,7 +215,7 @@ void clearStrips(){
 }
 
 void doCommit(){
-  int cStep = (millis() - orderStart) / deltaT;
+  int cStep = (millis() - stateStart) / deltaT;
   // Set strip
   for (int i=0; i<strips[commitModule].numPixels(); i++){
     if(((8 - i + cStep) % 12) / 4 == 0){
@@ -233,7 +242,7 @@ void checkAnalog(){
     return;
   }
   uint16_t val = 1023 - analogRead(4);
-  if(val + analogThreshold < analogLastValue ||  (val >= analogThreshold) && val - analogThreshold > analogLastValue){
+  if(val + ANALOG_THRESHOLD < analogLastValue ||  (val >= ANALOG_THRESHOLD) && val - ANALOG_THRESHOLD > analogLastValue){
     switchState(STATE_SELECTING);
     analogLastValue = val;
   }
@@ -244,23 +253,19 @@ void doSelect(){
   uint16_t val = 1023 - analogRead(4);
   selectedDev = val * nDevs / 1024;
   
-  if(val + analogThreshold < analogLastValue ||  (val >= analogThreshold) && val - analogThreshold > analogLastValue){
+  if(val + ANALOG_THRESHOLD < analogLastValue ||  (val >= ANALOG_THRESHOLD) && val - ANALOG_THRESHOLD > analogLastValue){
     //Serial.println(val); Serial.println(analogLastValue);Serial.println("");
     analogLastMoved = millis();
     analogLastValue = val;
   }
   
-
-  
   // Display ring
-  uint8_t nLeds = months.numPixels() / nDevs;
-  uint8_t remaining = months.numPixels() - (nLeds * nDevs);
 
   for(int i=0; i<months.numPixels(); i++){
     if(i < remaining){
       months.setPixelColor(i, 0);
     } else{
-      uint8_t dev = (i - remaining) / nLeds;
+      uint8_t dev = (i - remaining) / ledsPerDev;
       if(dev == selectedDev && val != 0) {
         months.setPixelColor(i, illum(colors[dev], 255));
       } else{
@@ -271,16 +276,28 @@ void doSelect(){
   months.show();
   
   // Check for time threshold
-  if(millis() - analogLastMoved >= analogTimeThreshold){
+  if(millis() - analogLastMoved >= ANALOG_TIME_THRESHOLD){
     if(val == 0){
       switchState(STATE_IDLE);
       Serial.println("idle");
     } else{
       Serial.println(selectedDev);
-      switchState(STATE_REPLAYING);
+      switchState(STATE_RECEIVING);
     }
   }
   
+}
+
+void doReceive(){
+  uint16_t illumFactor = (millis() - stateStart + 510) / 2 % 500;
+  if(illumFactor > 250){
+    illumFactor = 500 - illumFactor;
+  }
+  Serial.println(illumFactor);
+  for (uint8_t i=selectedDev * ledsPerDev + remaining; i<(selectedDev + 1) * ledsPerDev + remaining; i++){
+    months.setPixelColor(i, illum(colors[selectedDev], illumFactor, DEFAULT_BRIGHTNESS));
+  }
+  months.show();
 }
 
 void setup() {
@@ -314,6 +331,9 @@ void loop() {
       break;
     case STATE_SELECTING:
       doSelect();
+      break;
+    case STATE_RECEIVING:
+      doReceive();
       break;
     default:
       break;
